5c87ac9e6809b954d72778b529abed19d221589a
Gabriel Goes de Castro <gabriel.goes2107@gmail.com>
2023-05-15 20:50:45 -0300

    Array Random

--- VetorRandom.java
+++ VetorRandom.java
@@ -4,12 +4,12 @@ import java.util.Random;
 public class VetorRandom {
     public static void main (String [] args){
         Random random = new Random ();
-        int[] v = new int [5]; //Tamanho do vetor é de 5 espaços
+        int[] v = new int [6]; //Tamanho do vetor é de 5 espaços
         for (int i = 0; i < v.length; ++i) {
             v[i] = random.nextInt(101); //Valores aleatórios entre 0 e 100
         }
         for (int j = 0; j < v.length; ++j) {
-            System.out.println("Posição " + j + ": " + v[j] ); //Imprimir cada valor aleatório
+            System.out.println("Posição " + j + ": " + v[j]); //Imprimir cada valor aleatório
         }
     }
 }
