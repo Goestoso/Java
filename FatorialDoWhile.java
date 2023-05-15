9d1fca3f5d2e1a4cd91d259c2ec83929e129ac94
Gabriel Goes de Castro <gabriel.goes2107@gmail.com>
2023-05-15 20:53:39 -0300

    Class BigDecimal

--- FatorialDoWhile.java
+++ FatorialDoWhile.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.math.BigDecimal; // é uma classe que representa um número decimal preciso e de alta precisão. Diferentemente do double e do float, que são tipos de ponto flutuante e têm uma precisão limitada, o BigDecimal pode representar números com uma precisão muito alta e pode ser usado para realizar cálculos em que a precisão é crítica.
+import java.math.BigDecimal; //é uma classe que representa um número decimal preciso e de alta precisão. Diferentemente do double e do float, que são tipos de ponto flutuante e têm uma precisão limitada, o BigDecimal pode representar números com uma precisão muito alta e pode ser usado para realizar cálculos em que a precisão é crítica.
 
 //ler um número inteiro positivo e calcular e mostrar seu fatorial usando comando do-while
 public class FatorialDoWhile {
@@ -17,7 +17,7 @@ public class FatorialDoWhile {
         fat = BigDecimal.ONE;  // inicializando fat com 1, já que 0! = 1
         int cont = 2; // variável de controle do loop que vai multiplicar os números de 2 até n
         if (n < 2) { // caso especial para n = 0 e n = 1, em que o fatorial é 1
-            System.out.printf("Fatorial de %d = 1 \n", n);
+            System.out.printf("Fatorial de %d = 1 \n ", n);
         } else { 
             do { // loop que vai multiplicar os números de 2 até n
                 fat = fat.multiply(BigDecimal.valueOf(cont)); // multiplicando fat por cont (iniciando em 2) e armazenando o resultado em fat
